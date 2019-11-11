db20f7f7787b3cefa6783c0bdf356f3cf6ee4582
민경빈 <minkb01@gmail.com>
2019-11-11 23:29:33 +0900

    [ADD] 에니메이션 추가

--- src/containers/Carousel.js
+++ src/containers/Carousel.js
@@ -1,15 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
+import styled from "styled-components";
+
 import data from "../config/config";
 import Card from "../components/Card";
 
+const Wrapper = styled("div")`
+   width: 400px;
+   overflow-x: hidden;
+`;
+
+const Slider = styled("div")`
+   display: flex;
+   transform: translateX(-${props => props.scrollWidth}px);
+   transition: transform 0.45s;
+`;
+
 const Carousel = () => {
-   console.log(data);
+   const [index, setIndex] = useState(1);
 
    const cardList = data.map(item => (
       <Card key={item.id} url={item.imageUrl} />
    ));
 
-   return <div>{cardList}</div>;
+   return (
+      <Wrapper>
+         <Slider scrollWidth={400 * (index - 1)}>{cardList}</Slider>
+         <div>{data.find(element => element.id === index).title}</div>
+      </Wrapper>
+   );
 };
 
 export default Carousel;
