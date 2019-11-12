a9a6ff2c89bbd12e2514b1a915a5dcd1f5d9139f
mango906 <minkb01@gmail.com>
2019-11-12 18:40:20 +0900

    README.md 추가

--- src/App.js
+++ src/App.js
@@ -4,9 +4,12 @@ import Carousel from "./containers/Carousel";
 
 const GlobalStyle = createGlobalStyle`
     body {
+        display: flex;
         padding: 0;
         margin: 0;
         background-color: #282c34;
+        align-items: center;
+        justify-content: center;
     }
 `;
 
